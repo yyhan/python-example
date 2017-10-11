43f23c7b6ff94cd8c072ea9721ef2fafed8b9f8f
YFHan <446465265@qq.com>
2017-10-11 23:58:02 +0800

    增加正则测试代码

--- src/regex.py
+++ src/regex.py
@@ -19,9 +19,49 @@ if searchRes:
 else:
     print("search fail")
 
+print("==== url 测试 (match)  ====")
+
+urlRegex = re.compile("http[s]?://([a-zA-Z0-9\-]+\.)+([a-zA-Z0-9\-]+)")
+
+print(urlRegex.match("http://www.baidu.com"))
+print(urlRegex.match("https://www.baidu.com"))
+print(urlRegex.match("https://baidu.com"))
+print(urlRegex.match("https://baidu."))
+print(urlRegex.match("https://baidu"))
+print(urlRegex.match("https://baidu.com/"))
+print(urlRegex.match("www.baidu.com"))
+
+print("==== url 测试 (search) ====")
+
+print(urlRegex.search("http://www.baidu.com"))
+print(urlRegex.search("https://www.baidu.com"))
+print(urlRegex.search("https://baidu.com"))
+print(urlRegex.search("https://baidu."))
+print(urlRegex.search("https://baidu"))
+print(urlRegex.search("https://baidu.com/index.html"))
+print(urlRegex.search("www.baidu.com"))
+
+print("==== url 测试 (fullmatch) ====")
+
+print(urlRegex.fullmatch("http://www.baidu.com"))
+print(urlRegex.fullmatch("https://www.baidu.com"))
+print(urlRegex.fullmatch("https://baidu.com"))
+print(urlRegex.fullmatch("https://baidu."))
+print(urlRegex.fullmatch("https://baidu"))
+print(urlRegex.fullmatch("https://baidu.com/index.html"))
+print(urlRegex.fullmatch("www.baidu.com"))
+
+print("==== url 测试 (Match Objects) ====")
 
 
-# IndexError: no such group
-# print(searchRes.group(1))
+urlMatch = urlRegex.findall("http://www.baidu.com http://www.sina.com http://www.1688.com http://www.taobao.com")
+print(type(urlMatch))
+# findall 返回一个tuple(元组)数组
+for item in urlMatch[:]:
+    print(item)
+    print(type(item))
+    for s in item[:]:
+        print(s)
+    print("================")
 
 print('\n==== over ==== \n')
